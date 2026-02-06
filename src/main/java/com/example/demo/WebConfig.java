7084a3fd70464524e9a2155accfff0c812172d6c
wonu <938274@naver.com>
2026-02-06 19:29:33 +0900

    url 수정

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	final static String LOCAL_HANDLE = "/image/**";
 	final static String LOCAL_PATH = "file:///C:/Users/손경원/git/the-full-api/src/main/resources/static/image/";
-	// final static String LOCAL_PATH = "file:///C:/Users/wonu/git/the-full-api/src/main/resources/static/image/";
-	
+	// final static String LOCAL_PATH =
+	// "file:///C:/Users/wonu/git/the-full-api/src/main/resources/static/image/";
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -37,8 +37,8 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(REAL_HANDLE)
-		.addResourceLocations(REAL_PATH);
-    }
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(DEV_HANDLE)
+				.addResourceLocations(DEV_PATH);
+	}
 }
