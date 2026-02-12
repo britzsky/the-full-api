129d3da9bc21396bf6caab3bd87926892d434495
wonu <938274@naver.com>
2026-02-12 21:07:39 +0900

    전체수정

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	final static String LOCAL_HANDLE = "/image/**";
 	final static String LOCAL_PATH = "file:///C:/Users/손경원/git/the-full-api/src/main/resources/static/image/";
-	//final static String LOCAL_PATH = "file:///C:/Users/wonu/git/the-full-api/src/main/resources/static/image/";
+	// final static String LOCAL_PATH = "file:///C:/Users/wonu/git/the-full-api/src/main/resources/static/image/";
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -37,7 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(REAL_HANDLE)
-				.addResourceLocations(REAL_PATH);
+		registry.addResourceHandler(DEV_HANDLE)
+				.addResourceLocations(DEV_PATH);
 	}
 }
